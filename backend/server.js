3c2eefd03f66bde056e7f058d2b8c4bfcfc1b6f6
Nguyễn Minh Chiến <nguyenminhchienkupe@gmail.com>
2026-03-27 04:22:35 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,9 +33,9 @@ const dbConfig = {
 const pool = mysql.createPool(dbConfig);
 
 cloudinary.config({
-    cloud_name: 'CLOUDINARY_CLOUD_NAME',
-    api_key: 'CLOUDINARY_API_KEY',
-    api_secret: 'CLOUDINARY_API_SECRET'
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,   
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
