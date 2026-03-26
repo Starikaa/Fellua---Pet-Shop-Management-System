d35cc5f9731e613cf7455fb48ec56c939af5a2b5
Nguyễn Minh Chiến <nguyenminhchienkupe@gmail.com>
2026-03-27 00:29:33 +0700

    Update ProfilePage.js

--- frontend/src/components/ProfilePage.js
+++ frontend/src/components/ProfilePage.js
@@ -12,7 +12,7 @@ function ProfilePage({ user, setUser, onLogout, onBack }) {
 
     const handleUpdate = async () => {
         try {
-            const res = await axios.put('http://localhost:5000/api/user/update', {
+            const res = await axios.put(`${process.env.REACT_APP_API_URL}/api/user/update`, {
                 userId: user.user_id,
                 ...editData
             });
